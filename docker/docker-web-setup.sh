7d1fb02f35f3a31d0d874488a0a0dcf34559c22e
zjacokes <75634174+zjacokes@users.noreply.github.com>
2020-12-07 09:56:25 -0800

    Update docker-web-setup.sh

--- docker/docker-web-setup.sh
+++ docker/docker-web-setup.sh
@@ -2,7 +2,6 @@
 # from https://acesloris.readthedocs.io/en/latest/docs/wiki/00_SERVER_INSTALL_AND_CONFIGURATION/01_LORIS_Install/CentOS/README.html
 
 # Apache2
-sudo yum install httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd
 
@@ -15,7 +14,7 @@ sudo yum update
 # Make sure to have only one repository for PHP enabled
 sudo yum-config-manager --disable remi-php54
 sudo yum-config-manager --enable remi-php74
-sudo yum install php php-pdo php-pdo_mysql php-fpm php-gd php-json php-mbstring php-mysqlnd php-xml php-xmlrpc php-opcache
+sudo yum install php-pdo php-pdo_mysql php-fpm php-gd php-json php-mbstring php-mysqlnd php-xml php-xmlrpc php-opcache
 
 # NodeJS
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
